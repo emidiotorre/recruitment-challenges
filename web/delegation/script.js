efca65046afcd7e9d350bc1b2b1c936ceac2072f
emidiotorre <emidio.torre@gmail.com>
2017-11-14 13:55:49 +0100

    esercizi web risolti

--- web/delegation/script.js
+++ web/delegation/script.js
@@ -1,1 +1,18 @@
-// Your code here
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function(){
+    var items = document.querySelectorAll('.item');
+    items.forEach(function(item, i){
+        item.addEventListener('click', clickHandler);
+    });
+})
+
+function clickHandler(e) {
+    alert(e.target.textContent);
+}
--- web/throttle/index.js
+++ web/throttle/index.js
@@ -1,1 +1,23 @@
-// Your code here
+module.exports = throttlePromises;
+
+function throttlePromises(limit, arr) {
+  var i = 0;
+  var resultArray = new Array(arr.length);
+
+  function doNext() {
+    if (i < arr.length) {
+      var fnIndex = i++;
+      var next = arr[fnIndex];
+      return Promise.resolve(next())
+          .then(result => {  
+             resultArray[fnIndex] = result;
+             return;
+          }).then(doNext);
+    }
+  }
+  var listOfPromises = [];
+  while (i < limit && i < arr.length) {
+    listOfPromises.push(doNext());
+  }
+  return Promise.all(listOfPromises).then(() => resultArray);
+};
